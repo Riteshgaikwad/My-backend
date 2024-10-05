a14058d01d2ce2c548bd3ae2abe8ffad77ae0cf8
Riteshgaikwad <gaikwadritesh2003@gmail.com>
2024-10-05 14:59:43 +0530

    api handelling

--- src/index.js
+++ src/index.js
@@ -10,6 +10,14 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at port ${process.env.PORT}`);0
+    })
+})
+.catch((error)=>{
+    console.log("Mongo db Connection falied !!",error);
+})
 
 // BASIC APPROACH
 /*
